9e76e56c6e8b90b6b492293a1396f9895c603f09
Tushar Srivastava <tushar1404@gmail.com>
2019-12-24 16:19:50 +0530

    initial commit

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+// Export a function. Accept the base config as the only param.
+module.exports = async ({ config, mode }) => {
+  // `mode` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+
+  config.module.rules.push({
+    test: /\.scss/,
+    include: path.resolve(__dirname, '../'),
+    loaders: [
+      'style-loader',
+      {
+        loader: 'css-loader',
+        options: {
+          modules: {
+            localIdentName: '[name]_[local]--[hash:base64:5]',
+          },
+          importLoaders: 0,
+          localsConvention: 'camelCase',
+          sourceMap: true
+        },
+      },
+      'sass-loader',
+    ],
+  });
+  return config;
+};
