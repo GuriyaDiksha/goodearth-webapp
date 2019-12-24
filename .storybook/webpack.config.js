11ca2711c54186db19d9455ef9e0fda0ae8cf64b
Tushar Srivastava <tushar1404@gmail.com>
2019-12-24 17:00:14 +0530

    typescript support added

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,27 +1,28 @@
-const path = require('path');
+const path = require("path");
 
 // Export a function. Accept the base config as the only param.
 module.exports = async ({ config, mode }) => {
   // `mode` has a value of 'DEVELOPMENT' or 'PRODUCTION'
 
+  config.module.rules.push({ test: /\.(ts|tsx)$/, loader: "ts-loader" });
   config.module.rules.push({
     test: /\.scss/,
-    include: path.resolve(__dirname, '../'),
+    include: path.resolve(__dirname, "../"),
     loaders: [
-      'style-loader',
+      "style-loader",
       {
-        loader: 'css-loader',
+        loader: "css-loader",
         options: {
           modules: {
-            localIdentName: '[name]_[local]--[hash:base64:5]',
+            localIdentName: "[name]_[local]--[hash:base64:5]"
           },
           importLoaders: 0,
-          localsConvention: 'camelCase',
+          localsConvention: "camelCase",
           sourceMap: true
-        },
+        }
       },
-      'sass-loader',
-    ],
+      "sass-loader"
+    ]
   });
   return config;
 };
