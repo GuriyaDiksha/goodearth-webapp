5be5a68752819ac07b943edbc150c0e9418f8e52
darpanrawat <darpanrawat@gmail.com>
2020-01-09 15:45:33 +0530

    chnage product datatype

--- src/typings/product.ts
+++ src/typings/product.ts
@@ -17,7 +17,6 @@ export interface PLPProductItem {
   salesBadgeImage?: string;
   partial: boolean;
   markAs?: string[];
-  children?: string[];
   childAttributes?: PartialChildProductAttributes[] | ChildProductAttributes[];
 }
 
