1cdafdb974e4ee39479d34b88571cd8fee9580a4
Tushar Srivastava <tushar1404@gmail.com>
2020-05-09 15:38:45 +0530

    homepage ssr issue

--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useLayoutEffect, useState } from "react";
 import MakerEnhance from "maker-enhance";
 import styles from "./styles.scss";
 
 const Home: React.FC = () => {
+  const [mounted, setMounted] = useState(false);
+
+  useLayoutEffect(() => {
+    if (!mounted) {
+      setMounted(true);
+    }
+  });
   return (
     <div className={styles.makerTop}>
-      <MakerEnhance user="goodearth" />
+      {mounted && <MakerEnhance user="goodearth" />}
     </div>
   );
 };
