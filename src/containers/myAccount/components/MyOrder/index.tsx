ca6b84580021ea0214a969fc0e8b1814e7adc8c1
Manish Yadav <p-manishyadav@goodearth.in>
2020-05-27 16:26:28 +0530

    Add My Orders section

---
+++ src/containers/myAccount/components/MyOrder/index.tsx
@@ -0,0 +1,116 @@
+import React, { useState, useMemo } from "react";
+import cs from "classnames";
+import OnlineOrders from "./OnlineOrders";
+import InShopOrders from "./InShopOrders";
+import Loader from "components/Loader";
+import { MyOrderState } from "./typings";
+
+import bootstrapStyles from "../../../../styles/bootstrap/bootstrap-grid.scss";
+import globalStyles from "styles/global.scss";
+import styles from "../styles.scss";
+import { Link } from "react-router-dom";
+
+const PastOrders: React.FC = () => {
+  const [hasShoppedOnlineitems, setHasShoppedOnlineitems] = useState(true);
+  const [hasShoppedAtStore, setHasShoppedAtStore] = useState(false);
+  const [hasShopped, setHasShopped] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const switchToStoreOrders = () => {
+    setHasShoppedOnlineitems(false);
+    setHasShoppedAtStore(true);
+    setHasShopped(true);
+    setIsLoading(true);
+  };
+
+  const switchToOnlineOrders = () => {
+    setHasShoppedOnlineitems(true);
+    setHasShoppedAtStore(false);
+    setHasShopped(true);
+    setIsLoading(true);
+  };
+  const mainContent = (
+    <>
+      <div className={styles.orderTabs}>
+        <a
+          className={cs(styles.formSubheading, styles.orders, {
+            [styles.active]: hasShoppedOnlineitems
+          })}
+          onClick={switchToOnlineOrders}
+        >
+          ONLINE
+        </a>
+        <span className={styles.hrLine}>|</span>
+        <a
+          className={cs(styles.formSubheading, styles.orders, {
+            [styles.active]: hasShoppedAtStore
+          })}
+          onClick={switchToStoreOrders}
+        >
+          IN-SHOP
+        </a>
+      </div>
+      {!hasShopped && !isLoading && (
+        <div
+          className={cs(
+            styles.formSubheading,
+            globalStyles.op2,
+            styles.noPurchasesMessage,
+            globalStyles.voffset4
+          )}
+        >
+          <em>You haven't made any purchases yet</em>
+        </div>
+      )}
+      {hasShoppedOnlineitems && (
+        <OnlineOrders hasShopped={setHasShopped} isLoading={setIsLoading} />
+      )}
+      {hasShoppedAtStore && (
+        <InShopOrders hasShopped={setHasShopped} isLoading={setIsLoading} />
+      )}
+      {!hasShopped && !isLoading && (
+        <div className={styles.browseButton}>
+          <Link to="/">
+            <input
+              type="button"
+              className={globalStyles.ceriseBtn}
+              value="Browse"
+            />
+          </Link>
+        </div>
+      )}
+    </>
+  );
+  return (
+    <div className={bootstrapStyles.row}>
+      {isLoading && <Loader />}
+      <div
+        className={cs(
+          bootstrapStyles.col10,
+          bootstrapStyles.offset1,
+          bootstrapStyles.colMd8,
+          bootstrapStyles.offsetMd2
+        )}
+      >
+        <div className={bootstrapStyles.row}>
+          <div
+            className={cs(
+              bootstrapStyles.col10,
+              bootstrapStyles.offset1,
+              bootstrapStyles.colMd10,
+              bootstrapStyles.offsetMd1
+            )}
+          >
+            <div className={styles.formHeading}>My Orders</div>
+            <div className={styles.formSubheading}>
+              View all your past orders.
+            </div>
+            {mainContent}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PastOrders;
