57b309e21b66b3473598805623c78a00692f80e6
Tushar Srivastava <tushar1404@gmail.com>
2020-07-19 20:45:34 +0530

    http2 push changes

--- src/server/middleware/viewHandler.tsx
+++ src/server/middleware/viewHandler.tsx
@@ -16,6 +16,7 @@ import App from "containers/app";
 import { cssChunks } from "../staticAssets/cssChunks";
 import { LinkTag, Chunks } from "../typings";
 import config from "../../config";
+import { getPushHeader } from "../utils/response";
 
 const statsFile = path.resolve("dist/static/loadable-stats.json");
 
@@ -85,6 +86,19 @@ const viewHandler: Koa.Middleware = async function(ctx, next) {
         href: element.props.href
       });
     });
+
+    const scriptElements = extractor.getScriptElements();
+
+    const pushHeaders: string[] = [];
+    scriptElements.forEach((element: any) => {
+      if (element.props.src) {
+        pushHeaders.push(getPushHeader(element.props.src, "script"));
+      }
+    });
+
+    if (ctx.request.query.opt === "1") {
+      ctx.set("Link", pushHeaders.join(", "));
+    }
 
     await ctx.render("index", {
       state: JSON.stringify(store.getState()),
