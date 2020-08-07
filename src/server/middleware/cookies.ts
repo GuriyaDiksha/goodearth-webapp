794186f52718ffdf9ad9249bff782725842736bd
Darpan Rawat <darpanrawat@goodearth.in>
2020-08-07 17:42:31 +0530

    added cookies in store

--- src/server/middleware/cookies.ts
+++ src/server/middleware/cookies.ts
@@ -9,15 +9,24 @@ export default async function cookies(
 ) {
   const token = ctx.cookies.get("atkn");
   const currency: any = ctx.cookies.get("currency") || "INR";
+  const sessionId = ctx.cookies.get("sessionid");
   const store = ctx.store;
   ctx.customCookies = {
-    tkn: token
+    tkn: token,
+    sessionid: sessionId
   };
   store.dispatch(updateCurrency(currency));
   if (token) {
     store.dispatch(
       updateCookies({
         tkn: token
+      })
+    );
+  }
+  if (sessionId) {
+    store.dispatch(
+      updateCookies({
+        sessionid: sessionId
       })
     );
   }
