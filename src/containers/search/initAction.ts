daf46305f3903f578e38e2de5708a44fde1be4fb
Manish Yadav <manishyadav@goodearth.in>
2020-08-17 17:32:44 +0530

    Catch errors in intActions

--- src/containers/search/initAction.ts
+++ src/containers/search/initAction.ts
@@ -2,7 +2,9 @@ import { InitAction } from "typings/actions";
 import SearchService from "services/search";
 
 const initActionSearch: InitAction = async (dispatch, params, { search }) => {
-  await SearchService.onLoadSearchPage(dispatch, search);
+  await SearchService.onLoadSearchPage(dispatch, search).catch(err => {
+    console.log("Search Error API error", err);
+  });
 };
 
 export default initActionSearch;
