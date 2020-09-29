e0d2fe31ae5589e787bebbe28d82190b5408e16e
Manish Yadav <manishyadav@goodearth.in>
2020-09-29 14:50:26 +0530

    Make extraParams optional

--- src/containers/plp/typings.ts
+++ src/containers/plp/typings.ts
@@ -37,7 +37,7 @@ export type FilterProps = {
 };
 
 export type State = {
-  extraParams: { [x: string]: string };
+  extraParams?: { [x: string]: string };
   showmobileFilterList: boolean;
   filter: any;
   show: false;
