bcbbe7cdb69a9ba8a952fc0687b8b81a80d889c1
Manish Yadav <manishyadav@goodearth.in>
2020-10-12 18:30:17 +0530

    Add Homepage Maker popup

--- src/containers/base/index.tsx
+++ src/containers/base/index.tsx
@@ -11,15 +11,15 @@ import globalStyles from "styles/global.scss";
 import "styles/chat.css";
 import { AppState } from "reducers/typings";
 import { useSelector, useDispatch } from "react-redux";
-// import { updateComponent, updateModal } from "actions/modal";
-// import InfoPopup from "components/Popups/InfoPopup";
+import { updateComponent, updateModal } from "actions/modal";
+import MakerPopup from "components/Popups/MakerPopup";
 
 const BaseLayout: React.FC = () => {
   const location = useLocation();
   const { pathname } = location;
   const dispatch = useDispatch();
   const { currency } = useSelector((state: AppState) => state);
-  // const isSuspended = true;
+  const isSuspended = true;
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -42,12 +42,12 @@ const BaseLayout: React.FC = () => {
     }
   }, [pathname]);
 
-  // const setInfoPopupCookie = () => {
-  //   const cookieString =
-  //     "suspensioninfo=show; expires=Sat, 01 Jan 2050 00:00:01 UTC; path=/";
-  //   document.cookie = cookieString;
-  //   CookieService.setCookie("suspensioninfo", "show", 365);
-  // };
+  const setMakerPopupCookie = () => {
+    const cookieString =
+      "suspensioninfo=show; expires=Sat, 01 Jan 2050 00:00:01 UTC; path=/";
+    document.cookie = cookieString;
+    CookieService.setCookie("suspensioninfo", "show", 365);
+  };
 
   useEffect(() => {
     document.addEventListener("wheel", (e: WheelEvent) => {
@@ -69,16 +69,16 @@ const BaseLayout: React.FC = () => {
         );
       }
     });
-    // const popupCookie = CookieService.getCookie("suspensioninfo");
-    // if (isSuspended && popupCookie != "show") {
-    //   dispatch(
-    //     updateComponent(
-    //       <InfoPopup acceptCondition={setInfoPopupCookie} />,
-    //       true
-    //     )
-    //   );
-    //   dispatch(updateModal(true));
-    // }
+    const popupCookie = CookieService.getCookie("suspensioninfo");
+    if (isSuspended && popupCookie != "show") {
+      dispatch(
+        updateComponent(
+          <MakerPopup acceptCondition={setMakerPopupCookie} />,
+          true
+        )
+      );
+      dispatch(updateModal(true));
+    }
     LoginService.getClientIpCurrency()
       .then(curr => {
         if (curr != "error") {
