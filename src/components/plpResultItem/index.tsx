3b19bfcded041e5278423b5e4198b520290d6e93
Manish Yadav <manishyadav@goodearth.in>
2020-11-10 16:41:02 +0530

    Revert SP05_620 changes

--- src/components/plpResultItem/index.tsx
+++ src/components/plpResultItem/index.tsx
@@ -194,11 +194,11 @@ const PlpResultItem: React.FC<PLPResultItemProps> = (
             </span>
           </p>
         )}
-        {sizeExit && (
+        {sizeExit && !mobile && (
           <div
             className={cs(
               styles.productSizeList,
-              { [styles.productSizeListMobile]: mobile },
+              // { [styles.productSizeListMobile]: mobile },
               bootstyles.row
             )}
           >
