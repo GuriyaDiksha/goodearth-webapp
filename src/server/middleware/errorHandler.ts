c731bdc15abf746663446d15f2ee40c29378964b
Darpan Rawat <darpanrawat@goodearth.in>
2020-11-17 18:31:43 +0530

    change

--- src/server/middleware/errorHandler.ts
+++ src/server/middleware/errorHandler.ts
@@ -10,7 +10,8 @@ export default async function device(
     if (err?.response?.status == 500) {
       ctx.redirect("/error-page");
     }
-    ctx.status = 1001;
+
+    ctx.status = err?.response?.status;
     ctx.body = "Please Wait !!!!!!  Currently build is in process..";
     ctx.app.emit("error", err, ctx);
   }
