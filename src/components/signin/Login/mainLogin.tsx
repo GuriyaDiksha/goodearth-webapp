a386280556726ca9b2a7cb9a11d9d0186d8b9699
Manish Yadav <manishyadav@goodearth.in>
2021-02-01 15:26:56 +0530

    Enable continue button on email copy paste

--- src/components/signin/Login/mainLogin.tsx
+++ src/components/signin/Login/mainLogin.tsx
@@ -282,11 +282,13 @@ class MainLogin extends React.Component<Props, loginState> {
           if (this.state.msg !== "") {
             this.setState({
               msg: "",
-              highlight: false,
-              showerror: "",
-              isLoginDisabled: false
+              highlight: false
             });
           }
+          this.setState({
+            showerror: "",
+            isLoginDisabled: false
+          });
         }
       }
     }
