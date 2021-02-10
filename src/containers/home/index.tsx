3cdc91a801bd6c6a8b649ad6c7e438cdaf5b82f9
Darpan Rawat <darpanrawat@goodearth.in>
2021-02-10 12:45:59 +0530

    remove unwanted page

--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -1,33 +1,10 @@
-import React, { useEffect, useLayoutEffect, useState } from "react";
-//import MakerEnhance from "maker-enhance";
+import React, { useEffect } from "react";
 import styles from "./styles.scss";
-import globalStyles from "../../styles/global.scss";
-import { useLocation } from "react-router";
-import { AppState } from "reducers/typings";
-import { useSelector } from "react-redux";
 import Section1 from "./section1";
 import Section2 from "./section2";
 import Section3 from "./section3";
-// import { Link } from "react-router-dom";
-// import INRBanner from "../../images/banner/INRBanner.jpg";
-// import USDGBPBanner from "../../images/banner/USDGBPBanner.jpg";
-// import INRBannerMobile from "../../images/banner/INRBannerMobile.jpg";
-// import USDGBPBannerMobile from "../../images/banner/USDGBPBannerMobile.jpg";
 
 const Home: React.FC = () => {
-  const [mounted, setMounted] = useState(false);
-  const location = useLocation();
-  const {
-    currency
-    // device: { mobile }
-  } = useSelector((state: AppState) => state);
-  const { makerReloadToggle } = useSelector((state: AppState) => state.info);
-  useLayoutEffect(() => {
-    setMounted(false);
-    setTimeout(() => {
-      setMounted(true);
-    }, 100);
-  }, [currency, makerReloadToggle]);
   useEffect(() => {
     setTimeout(() => {
       window.scrollTo(0, 0);
@@ -42,29 +19,6 @@ const Home: React.FC = () => {
     });
   }, []);
 
-  useEffect(() => {
-    const noContentContainerElem = document.getElementById(
-      "no-content"
-    ) as HTMLDivElement;
-    if (location.pathname == "/corporate-gifts-catalogue") {
-      if (
-        noContentContainerElem.classList.contains(globalStyles.contentContainer)
-      ) {
-        noContentContainerElem.classList.remove(globalStyles.contentContainer);
-      }
-    } else if (
-      !noContentContainerElem.classList.contains(globalStyles.contentContainer)
-    ) {
-      noContentContainerElem.classList.add(globalStyles.contentContainer);
-    }
-  }, [location.pathname]);
-  // const mobileBannerImagePath =
-  //   currency == "INR" ? INRBannerMobile : USDGBPBannerMobile;
-  // const desktopBannerImagePath = currency == "INR" ? INRBanner : USDGBPBanner;
-  // const bannerUrl =
-  //   currency == "INR"
-  //     ? "/collection/women_ruh-fiza_457/"
-  //     : "/catalogue/category/women/sale-int_271/?source=plp&category_shop=Apparel+%3E+Sale+%3E+50%25";
   return (
     <div className={styles.makerTop}>
       <Section1 />
