0711cb6c3fdaaae36b48002f2521bfdf680670fc
Manish Yadav <manishyadav@goodearth.in>
2021-02-18 17:24:37 +0530

    Prevent duplicate initaction requests for homepage

--- src/containers/checkout/initAction.ts
+++ src/containers/checkout/initAction.ts
@@ -3,9 +3,10 @@ import { InitAction } from "typings/actions";
 // import Api from "services/api";
 import AddressService from "../../services/address";
 
-const initActionCheckout: InitAction = async dispatch => {
+const initActionCheckout: InitAction = async store => {
   // code
   // Api.getCurrency(dispatch);
+  const dispatch = store.dispatch;
   AddressService.fetchPinCodeData(dispatch)
     .then(data => {
       const pinCodeList = Object.keys(data);
