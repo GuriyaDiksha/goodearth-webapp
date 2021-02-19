2f53879b3b8c21cdceb0d50a5e550c4aede18d5d
Darpan Rawat <darpanrawat@goodearth.in>
2021-02-19 18:47:54 +0530

    code for 404 in pdp

--- src/typings/actions.ts
+++ src/typings/actions.ts
@@ -3,12 +3,14 @@ import { RouteParams } from "src/routes/typings";
 import { Location } from "history";
 import { PageMetaRequest } from "services/meta/typings";
 import { Currency } from "./currency";
+import { History } from "history";
 
 export type InitAction = (
   store: Store,
   params: RouteParams,
   location: Location,
-  currency?: Currency
+  currency?: Currency,
+  history?: History
 ) => void;
 
 export type MetaAction = (location: Location) => Partial<PageMetaRequest>;
