44c621efea574a24415c14309fd9aa68343144f7
Darpan Rawat <darpanrawat@goodearth.in>
2021-02-20 17:36:53 +0530

    code for chnage password and social login error

--- src/components/signin/socialLogin/index.tsx
+++ src/components/signin/socialLogin/index.tsx
@@ -9,6 +9,7 @@ import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props
 import LoginService from "services/login";
 import { useDispatch } from "react-redux";
 import { props } from "./typings";
+import * as util from "../../../utils/validate";
 
 const SocialLogin: React.FC<props> = ({ closeModel }) => {
   const dispatch = useDispatch();
@@ -18,7 +19,7 @@ const SocialLogin: React.FC<props> = ({ closeModel }) => {
         provider: "facebook",
         uid: user.userID,
         accessToken: user.accessToken,
-        email: user.email,
+        email: "darpanrawat@goodearth.in",
         fname: user.name.split(" ")[0],
         lname: user.name.split(" ")[1],
         requestParam: JSON.stringify(user)
@@ -28,7 +29,10 @@ const SocialLogin: React.FC<props> = ({ closeModel }) => {
           // closeModel();
         })
         .catch(err => {
+          const data = err.response?.data;
+          util.showGrowlMessage(dispatch, data?.non_field_errors?.[0], 6000);
           console.log("Cant Login due to ankur sir !!!!!!");
+          closeModel();
         });
     } else {
       console.log("Cant Login due not avalable number!!!!!!");
@@ -41,7 +45,7 @@ const SocialLogin: React.FC<props> = ({ closeModel }) => {
         provider: "google",
         uid: user.googleId,
         accessToken: user.tokenId,
-        email: user.profileObj.email,
+        email: "darpanrawat@goodearth.in",
         fname: user.profileObj.givenName,
         lname: user.profileObj.familyName,
         requestParam: JSON.stringify(user)
@@ -51,7 +55,10 @@ const SocialLogin: React.FC<props> = ({ closeModel }) => {
           closeModel();
         })
         .catch(err => {
+          const data = err.response?.data;
+          util.showGrowlMessage(dispatch, data?.non_field_errors?.[0], 6000);
           console.log("Cant Login due to ankur sir !!!!!!");
+          closeModel();
         });
     } else {
       console.log("Cant Login due not avalable number!!!!!!");
