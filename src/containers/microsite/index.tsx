c9a325f2494c0c56ee08ef65d48a6ca757386fb0
Darpan Rawat <darpanrawat@goodearth.in>
2021-04-09 12:44:33 +0530

    code for microsite

--- src/containers/microsite/index.tsx
+++ src/containers/microsite/index.tsx
@@ -1,17 +1,33 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { useHistory } from "react-router";
 import { AppState } from "reducers/typings";
 import styles from "./styles.scss";
 
-const Microsite: React.FC<{ url: string }> = props => {
+const Microsite: React.FC = (props: any) => {
+  const history = useHistory();
   const {
+    device: { mobile },
     info: { microUrl }
   } = useSelector((state: AppState) => state);
-
+  if (!props.id) {
+    history.replace(`/microsite/${microUrl}`);
+  }
   return (
     <div className={styles.makerTop}>
+      <div
+        className={styles.subHeading}
+        onClick={() => {
+          history.push("/account/cerise");
+        }}
+      >
+        {mobile ? "< Back" : "< Back to My Account"}
+      </div>
       <iframe
-        src={`https://goodearthindia.mloyalretail.com/microsite/default.asp?cid=${microUrl}`}
+        className={styles.iframeBox}
+        src={`https://goodearthindia.mloyalretail.com/microsite/default.asp?cid=${
+          props.id ? props.id : microUrl
+        }`}
       ></iframe>
     </div>
   );
