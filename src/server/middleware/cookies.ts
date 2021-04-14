bf2910543c2b38ddccc0664069f983bcda219613
Manish Yadav <manishyadav@goodearth.in>
2021-04-14 18:45:03 +0530

    Comment console log

--- src/server/middleware/cookies.ts
+++ src/server/middleware/cookies.ts
@@ -33,9 +33,9 @@ export default async function cookies(
   ) {
     // dispatch(updateComponent(POPUP.CURRENCY, null, true));
     // dispatch(updateModal(true));
-    console.log("ctx object:");
-    console.log(ctx);
-    console.log(ctx.request.headers);
+    // console.log("ctx object:");
+    // console.log(ctx);
+    // console.log(ctx.request.headers);
   }
   ctx.customCookies = {
     tkn: token,
