39343cc1b2151c62841055c985ed9528fd2f851d
Manish Yadav <manishyadav@goodearth.in>
2021-04-30 22:23:33 +0530

    Add datalayer codes for mega menu

--- src/components/header/templates/menuComponents/L3.tsx
+++ src/components/header/templates/menuComponents/L3.tsx
@@ -8,8 +8,32 @@ import styles from "../styles.scss";
 
 type Props = {
   data: MenuComponent[];
+  templateType: string;
+  l1: string;
+  l2: string;
+  onHeaderMegaMenuClick: ({
+    l1,
+    l2,
+    l3,
+    clickUrl1,
+    clickUrl2,
+    clickUrl3,
+    template,
+    img2,
+    img3,
+    cta,
+    subHeading
+  }: {
+    [x: string]: string;
+  }) => void;
 };
-const L3: React.FC<Props> = ({ data }) => {
+const L3: React.FC<Props> = ({
+  data,
+  templateType,
+  l1,
+  l2,
+  onHeaderMegaMenuClick
+}) => {
   return (
     <>
       {data.map((menuComponent, index) => {
@@ -18,11 +42,35 @@ const L3: React.FC<Props> = ({ data }) => {
           <div key={index} className={styles.block}>
             {" "}
             {componentData.link ? (
-              <Link className={styles.l3} to={componentData.link}>
+              <Link
+                className={styles.l3}
+                to={componentData.link}
+                onClick={() =>
+                  onHeaderMegaMenuClick({
+                    l1,
+                    l2,
+                    l3: componentData.text,
+                    clickUrl3: componentData.link,
+                    template: templateType
+                  })
+                }
+              >
                 {componentData.text}
               </Link>
             ) : (
-              <div className={styles.l3}>{componentData.text}</div>
+              <div
+                className={styles.l3}
+                onClick={() =>
+                  onHeaderMegaMenuClick({
+                    l1,
+                    l2,
+                    l3: componentData.text,
+                    template: templateType
+                  })
+                }
+              >
+                {componentData.text}
+              </div>
             )}
           </div>
         );
