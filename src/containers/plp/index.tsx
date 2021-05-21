666633d66a371759ff581036e543b55384fcd2af
Manish Yadav <manishyadav@goodearth.in>
2021-05-21 14:51:58 +0530

    Add animation in sort refine menu

--- src/containers/plp/index.tsx
+++ src/containers/plp/index.tsx
@@ -397,9 +397,12 @@ class PLP extends React.Component<
             id="filter_by"
             className={
               mobile
-                ? this.state.mobileFilter
-                  ? cs(bootstrap.col12, styles.mobileFilterMenu)
-                  : globalStyles.hidden
+                ? cs(
+                    { [globalStyles.active]: this.state.mobileFilter },
+                    bootstrap.col12,
+                    styles.mobileFilterMenu,
+                    globalStyles.hideLeft
+                  )
                 : cs(bootstrap.colMd2, styles.filterSticky)
             }
           >
