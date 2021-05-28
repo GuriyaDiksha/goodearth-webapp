ec2e2425fe3837dde6c2ff83b78c4923da38e799
Manish Yadav <manishyadav@goodearth.in>
2021-05-28 13:01:30 +0530

    Fix animation bugs

--- src/components/PlpDropDown/index.tsx
+++ src/components/PlpDropDown/index.tsx
@@ -76,7 +76,7 @@ const PlpDropdownMenu = ({
       <div
         className={cs(
           {
-            // [globalStyles.hidden]: menuOpen
+            [globalStyles.hidden]: menuOpen
           },
           bootstrap.col12,
           {
@@ -128,9 +128,11 @@ const PlpDropdownMenu = ({
             // [globalStyles.hidden]: !menuOpen
           },
           bootstrap.col12,
-          styles.productNumber
+          styles.productNumber,
+          { [styles.noPadding]: !menuOpen }
         )}
       >
+        {/* headers when menu is open */}
         <div className={cs({ [styles.mobileFilterSortBg]: showmobileSort })}>
           <div
             className={cs(styles.mobileFilterHeader, globalStyles.hideLeft, {
