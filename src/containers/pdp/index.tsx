c1ac305d10a2b822eeac3bce550d6f6bbdc1a4b3
Manish Yadav <manishyadav@goodearth.in>
2021-06-17 14:23:00 +0530

    Add alt attribute in product images

--- src/containers/pdp/index.tsx
+++ src/containers/pdp/index.tsx
@@ -115,7 +115,8 @@ class PDPContainer extends React.Component<Props, State> {
         images: images,
         startIndex: index,
         mobile: mobile,
-        changeModalState: changeModalState
+        changeModalState: changeModalState,
+        alt: this.props.data.altText
       },
       true
     );
@@ -444,6 +445,7 @@ class PDPContainer extends React.Component<Props, State> {
           id={`img-${image.id}`}
         >
           <PdpImage
+            alt={this.props.data.altText || this.props.data.title}
             {...image}
             index={index}
             onClick={this.onImageClick}
@@ -721,6 +723,7 @@ class PDPContainer extends React.Component<Props, State> {
                     // onClick={gtmProductClick}
                   > */}
                   <LazyImage
+                    alt={data.altText || data.title}
                     aspectRatio="62:93"
                     src={
                       data.lookImageUrl ||
@@ -899,6 +902,7 @@ class PDPContainer extends React.Component<Props, State> {
         return (
           <div key={id} className={globalStyles.relative}>
             <LazyImage
+              alt={data.altText || data.title}
               aspectRatio="62:93"
               src={productImage.replace("/Micro/", "/Medium/")}
               className={globalStyles.imgResponsive}
@@ -973,6 +977,7 @@ class PDPContainer extends React.Component<Props, State> {
             >
               <div className={bootstrap.row}>
                 <VerticalImageSelector
+                  alt={data.altText || data.title}
                   images={images}
                   className={cs(
                     bootstrap.colSm10,
