3f4c5dd19dc44707827db4f3caea9f6e259d691e
Darpan Rawat <darpanrawat@maheshsingh.goodearth.net>
2021-10-12 10:14:48 +0530

    code for filler msg

--- src/typings/product.ts
+++ src/typings/product.ts
@@ -65,6 +65,8 @@ export interface Product<T = ProductID> extends PartialProductItem {
   looksProducts?: PLPProductItem[];
   lookImageUrl?: string;
   groupedProducts?: GroupedProductItem[];
+  showFillerMessage?: boolean;
+  fillerUrl?: string;
 }
 
 export interface PartialChildProductAttributes {
