03860bd12cde80165019ecbae1ab8686eb997741
Darpan Rawat <darpanrawat@maheshsingh.goodearth.net>
2021-10-14 16:13:47 +0530

    encode url second

--- src/containers/plp/filterList.tsx
+++ src/containers/plp/filterList.tsx
@@ -517,7 +517,7 @@ class FilterList extends React.Component<Props, State> {
       const urlParams = new URLSearchParams(this.props.history.location.search);
       const categoryShop = urlParams.get("category_shop");
       if (categoryShop) {
-        fetchPlpTemplates(categoryShop);
+        fetchPlpTemplates(encodeURIComponent(decodeURI(categoryShop)));
       }
     }
   };
