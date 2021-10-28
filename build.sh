1bb5865327357fcada8ddc737ef93545d225e8b3
Darpan Rawat <darpanrawat@goodearth.in>
2021-10-28 14:46:42 +0530

    name change

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 rm -rf ./dist
 
 yarn install --ignore-optional --production=false
-cp serviceworker.js dist 
 yarn generateFonts
 APP=client NODE_ENV=production ./node_modules/.bin/webpack --config webpack/config.js -a client
 APP=server NODE_ENV=production ./node_modules/.bin/webpack --config webpack/config.js -a server
+cp serviceworker.js dist
