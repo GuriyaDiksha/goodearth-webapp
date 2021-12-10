b45ee5b14943de51498be65bf2c579725f5f5f31
Darpan Rawat <darpanrawat@goodearth.in>
2021-12-10 11:08:35 +0530

    comment the code of moengage

--- src/containers/search/index.tsx
+++ src/containers/search/index.tsx
@@ -235,10 +235,10 @@ class Search extends React.Component<
             }
           }
         });
-        Moengage.track_event("search", {
-          keyword: product.name,
-          searchClicked: true
-        });
+        // Moengage.track_event("search", {
+        //   keyword: product.name,
+        //   searchClicked: true
+        // });
       }
     } catch (err) {
       console.log("Search GTM error");
