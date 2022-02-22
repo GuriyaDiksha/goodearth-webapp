9deec30ddb75a19529c98b4a66f60ec400c4514f
Manish Yadav <manishyadav@goodearth.in>
2022-02-22 09:05:27 +0530

    Add an overlay with loader to prevent any click action by user while loading

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -4,8 +4,15 @@ import cs from "classnames";
 import styles from "./styles.scss";
 
 const Loader: React.FC = memo(() => {
+  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    event.stopPropagation();
+  };
   return (
-    <div className={cs(styles.cssloadContainer, styles.fullLoadWrap)}>
+    <div
+      className={cs(styles.cssloadContainer, styles.fullLoadWrap)}
+      onClickCapture={handleClick}
+    >
       <span className={styles.cssloadLoading}>
         <i></i>
         <i></i>
