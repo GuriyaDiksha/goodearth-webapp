06743cda8a928056fb7933b4d6c1cec7283bb1bb
Manish Yadav <manishyadav@goodearth.in>
2022-03-21 11:25:08 +0530

    Fix Cart page event bugs

--- src/components/Bag/index.tsx
+++ src/components/Bag/index.tsx
@@ -9,6 +9,7 @@ import { NavLink, Link } from "react-router-dom";
 import { currencyCodes } from "constants/currency";
 import { Dispatch } from "redux";
 import BasketService from "services/basket";
+import CookieService from "services/cookie";
 import { connect } from "react-redux";
 import { AppState } from "reducers/typings";
 import * as util from "../../utils/validate";
@@ -53,7 +54,7 @@ class Bag extends React.Component<Props, State> {
       "Page Url": location.href,
       "Page Type": util.getPageType(),
       "Login Status": this.props.isLoggedIn ? "logged in" : "logged out",
-      "Page referrer url": document.referrer || ""
+      "Page referrer url": CookieService.getCookie("prevUrl") || ""
     });
   };
   componentWillUnmount = () => {
