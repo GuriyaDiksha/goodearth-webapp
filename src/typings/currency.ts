a9b525d54ce16b307e11241e1d83260908df7be8
Darpan Rawat <darpanrawat@goodearth.in>
2022-04-22 13:18:04 +0530

    code for currency update

--- src/typings/currency.ts
+++ src/typings/currency.ts
@@ -1,8 +1,9 @@
-export type Currency = "USD" | "INR" | "GBP" | "AED";
+export type Currency = "USD" | "INR" | "GBP" | "AED" | "SGD";
 
 export const currencyCode: { [x in Currency]: any } = {
   INR: [8377],
   USD: [36],
   GBP: [163],
-  AED: [1583, 46, 1573]
+  AED: [65, 69, 68],
+  SGD: [83, 36]
 };
