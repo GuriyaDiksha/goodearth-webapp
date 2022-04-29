9b55ad012c9c2011bf95e1e4b66f7ba950b061b7
Darpan Rawat <darpanrawat@goodearth.in>
2022-04-29 17:15:54 +0530

    cusion filler code added

--- src/components/Cushion/index.tsx
+++ src/components/Cushion/index.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import styles from "./styles.scss";
 import cs from "classnames";
-import { CartProps, State } from "./typings";
+import { State } from "./typings";
 import iconStyles from "../../styles/iconFonts.scss";
 import globalStyles from "../../styles/global.scss";
-import LineItems from "./Item";
-import { NavLink, Link } from "react-router-dom";
+// import { NavLink, Link } from "react-router-dom";
 import { currencyCodes } from "constants/currency";
 import { Dispatch } from "redux";
 import BasketService from "services/basket";
 import { connect } from "react-redux";
 import { AppState } from "reducers/typings";
-import * as util from "../../utils/validate";
+import LazyImage from "components/LazyImage";
+// import * as util from "../../utils/validate";
 
 const mapDispatchToProps = (dispatch: Dispatch) => {
   return {
@@ -25,11 +25,11 @@ const mapStateToProps = (state: AppState) => {
   return {
     isSale: state.info.isSale,
     customerGroup: state.user.customerGroup,
-    isLoggedIn: state.user.isLoggedIn
+    isLoggedIn: state.user.isLoggedIn,
+    filler: state.filler
   };
 };
-type Props = CartProps &
-  ReturnType<typeof mapDispatchToProps> &
+type Props = ReturnType<typeof mapDispatchToProps> &
   ReturnType<typeof mapStateToProps>;
 class CushionBag extends React.Component<Props, State> {
   constructor(props: Props) {
@@ -45,227 +45,62 @@ class CushionBag extends React.Component<Props, State> {
 
   componentDidMount = () => {
     document.body.classList.add(globalStyles.noScroll);
-    try {
-      const skuList = this.props.cart.lineItems.map(
-        item => item.product.childAttributes?.[0].sku
-      );
-      dataLayer.push({
-        "Event Category": "GA Ecommerce",
-        "Event Action": "Cart Summary Page",
-        "Event Label": skuList.length > 0 ? skuList.join(",") : "",
-        "Time Stamp": new Date().toISOString(),
-        "Page Url": location.href,
-        "Page Type": util.getPageType(),
-        "Login Status": this.props.isLoggedIn ? "logged in" : "logged out",
-        "Page referrer url": location.href
-      });
-    } catch (err) {
-      console.log(err);
-    }
   };
+
   componentWillUnmount = () => {
     document.body.classList.remove(globalStyles.noScroll);
   };
 
-  hasOutOfStockItems = () => {
-    const items = this.props.cart.lineItems;
-    if (items) {
-      for (let i = 0; i < items.length; i++) {
-        const item = items[i];
-        if (item.product.stockRecords[0].numInStock < 1) {
-          return true;
-        }
-      }
+  getsection() {
+    const images: any[] = this.getProductImagesData();
+    let mobileSlides: any[] = [];
+    if (images.length > 0) {
+      mobileSlides = images?.map((url, i: number) => {
+        return (
+          <div key={"filler" + i} className={globalStyles.relative}>
+            <LazyImage
+              aspectRatio="62:93"
+              src={url.replace("/Micro/", "/Medium/")}
+              className={globalStyles.imgResponsive}
+            />
+          </div>
+        );
+      });
     }
-    return false;
-  };
 
-  getItemsCount() {
-    let count = 0;
-    const items = this.props.cart.lineItems;
-    for (let i = 0; i < items.length; i++) {
-      count = count + items[i].quantity;
-    }
-    return count;
+    return <div>{mobileSlides}</div>;
   }
 
-  getItems() {
-    const {
-      cart: { lineItems },
-      currency
-    } = this.props;
-
-    const item = lineItems.map(item => {
-      return (
-        <LineItems
-          key={item.id}
-          {...item}
-          currency={currency}
-          saleStatus={this.props.isSale}
-          toggleBag={this.props.toggleBag}
-        />
-      );
-    });
-    return item.length > 0 ? (
-      item
-    ) : (
-      <p className={cs(globalStyles.marginT20, globalStyles.textCenter)}>
-        No items added to bag.
-      </p>
-    );
-  }
-  removeOutOfStockItems = () => {
-    this.props.removeOutOfStockItems();
+  getProductImagesData = () => {
+    const { data } = this.props.filler;
+    let sliderImages;
+    if (data) {
+      sliderImages = data.plpSliderImages;
+    }
+    return sliderImages ? sliderImages : [];
   };
 
   getFooter() {
-    if (this.props.cart) {
-      const discountAmount = this.props.cart.offerDiscounts
-        .map(discount => {
-          return +discount.amount;
-        })
-        .reduce((partialSum, a) => partialSum + a, 0);
-
+    if (this.props.filler.show) {
       return (
         <div className={styles.bagFooter}>
-          {this.hasOutOfStockItems() && (
-            <div
-              className={cs(
-                globalStyles.errorMsg,
-                globalStyles.lineHt10,
-                styles.containerCost,
-                globalStyles.linkTextUnderline
-              )}
-              onClick={this.removeOutOfStockItems}
-              style={{ display: "inline-block" }}
-            >
-              Remove all Items out of stock
-            </div>
-          )}
-          {discountAmount > 0 && (
-            <div
-              className={cs(
-                globalStyles.flex,
-                globalStyles.gutterBetween,
-                styles.containerCost
-              )}
-            >
-              <div className={cs(styles.totalPrice, globalStyles.bold)}>
-                Discount
-              </div>
-              <div className={globalStyles.textRight}>
-                <h5 className={cs(styles.totalPrice, globalStyles.bold)}>
-                  (-)
-                  {String.fromCharCode(...currencyCodes[this.props.currency])}
-                  &nbsp;
-                  {parseFloat(discountAmount.toString()).toFixed(2)}
-                </h5>
-              </div>
-            </div>
-          )}
           <div
             className={cs(
               globalStyles.flex,
               globalStyles.gutterBetween,
               styles.containerCost
             )}
-          >
-            <div className={cs(styles.totalPrice, globalStyles.bold)}>
-              TOTAL*
-            </div>
-            <div className={globalStyles.textRight}>
-              <h5 className={cs(styles.totalPrice, globalStyles.bold)}>
-                {String.fromCharCode(...currencyCodes[this.props.currency])}
-                &nbsp;
-                {parseFloat(this.props.cart.total.toString()).toFixed(2)}
-              </h5>
-              <p className={styles.subtext}>
-                *Excluding estimated cost of shipping
-              </p>
-            </div>
-          </div>
-          {/* {amount.name && (
-            <div
-              className={cs(
-                globalStyles.flex,
-                globalStyles.gutterBetween,
-                styles.containerCost
-              )}
-            >
-              <div className={cs(styles.disPrice)}>EMP Discount</div>
-              <div className={globalStyles.textRight}>
-                <h5 className={cs(styles.disPrice, globalStyles.bold)}>
-                  (-)
-                  {String.fromCharCode(...currencyCodes[this.props.currency])}
-                  &nbsp;
-                  {parseFloat(amount.amount?.toString()).toFixed(2)}
-                </h5>
-              </div>
-            </div>
-          )}
-          {amount.name && <div
-            className={cs(
-              globalStyles.flex,
-              globalStyles.gutterBetween,
-              styles.containerCost
-            )}
-          >
-            <div className={cs(styles.totalPrice, globalStyles.bold)}>
-              TOTAL
-            </div>
-            <div className={globalStyles.textRight}>
-              <h5 className={cs(styles.totalPrice, globalStyles.bold)}>
-                {String.fromCharCode(...currencyCodes[this.props.currency])}
-                &nbsp;
-                {parseFloat(
-                  this.props.cart.amountPayable?.toString() || ""
-                ).toFixed(2)}
-              </h5>
-            </div>
-          </div>
-          } */}
+          ></div>
 
           <div className={cs(globalStyles.flex, styles.bagFlex)}>
-            <div className={cs(styles.iconCart, globalStyles.pointer)}>
-              <Link to="/cart">
-                <div className={styles.innerDiv}>
-                  <div className={styles.cartIconDiv}>
-                    <i
-                      className={cs(
-                        iconStyles.icon,
-                        iconStyles.iconCart,
-                        globalStyles.cerise
-                      )}
-                    ></i>
-                  </div>
-                  <span className={styles.viewBag}>VIEW SHOPPING BAG</span>
-                </div>
-              </Link>
+            <div className={cs(styles.iconCart, globalStyles.pointer)}></div>
+            <div>
+              <button
+                className={cs(globalStyles.ceriseBtn, globalStyles.disabledBtn)}
+              >
+                Add To Bag
+              </button>
             </div>
-            {this.canCheckout() ? (
-              <NavLink key="checkout" to="/order/checkout">
-                <button
-                  onClick={this.chkshipping}
-                  className={cs(globalStyles.ceriseBtn, {
-                    [globalStyles.disabledBtn]: !this.canCheckout()
-                  })}
-                >
-                  PROCEED TO CHECKOUT
-                </button>
-              </NavLink>
-            ) : (
-              <div>
-                <button
-                  disabled={!this.canCheckout()}
-                  className={cs(
-                    globalStyles.ceriseBtn,
-                    globalStyles.disabledBtn
-                  )}
-                >
-                  PROCEED TO CHECKOUT
-                </button>
-              </div>
-            )}
           </div>
         </div>
       );
@@ -278,97 +113,23 @@ class CushionBag extends React.Component<Props, State> {
     document.cookie = cookieString;
   }
 
-  chkshipping = (event: React.MouseEvent) => {
-    // if (window.ischeckout) {
-    //     return false;
-    // }
-    // const self = this;
-    const {
-      totalWithoutShipping,
-      freeShippingThreshold,
-      freeShippingApplicable
-    } = this.props.cart;
-    if (this.state.isSuspended) {
-      this.resetInfoPopupCookie();
-    }
-    if (
-      !this.state.freeShipping &&
-      totalWithoutShipping &&
-      totalWithoutShipping >= freeShippingThreshold &&
-      totalWithoutShipping < freeShippingApplicable &&
-      this.props.currency == "INR" &&
-      this.props.cart.shippable
-    ) {
-      this.props.showShipping(
-        freeShippingApplicable - parseFloat(totalWithoutShipping.toString()),
-        freeShippingApplicable
-      );
-      event.preventDefault();
-    }
-  };
-
-  canCheckout = () => {
-    // if (pathname.indexOf("checkout") > -1) {
-    //   return false;
-    // }
-    // this.amountLeft = 50000 - this.props.cart.subTotal;
-    const {
-      totalWithoutShipping,
-      freeShippingThreshold,
-      freeShippingApplicable
-    } = this.props.cart;
-    if (
-      !this.props.cart.lineItems ||
-      this.hasOutOfStockItems() ||
-      this.props.cart.lineItems.length == 0
-    ) {
-      return false;
-    }
-    if (!this.props.cart.shippable && this.state.shipping == true) {
-      this.setState({
-        shipping: false
-      });
-    } else if (
-      totalWithoutShipping &&
-      totalWithoutShipping >= freeShippingThreshold &&
-      totalWithoutShipping < freeShippingApplicable &&
-      this.state.shipping == false &&
-      this.props.currency == "INR" &&
-      this.props.cart.shippable
-    ) {
-      this.setState({
-        shipping: true
-      });
-    } else if (
-      totalWithoutShipping &&
-      (totalWithoutShipping < freeShippingThreshold ||
-        totalWithoutShipping >= freeShippingApplicable) &&
-      this.state.shipping
-    ) {
-      this.setState({
-        shipping: false
-      });
-    }
-    return true;
-  };
-
   render() {
+    const {
+      filler: { show }
+    } = this.props;
     return (
       <div>
         <div
-          className={cs(
-            styles.bagBackdrop,
-            this.props.active ? styles.active : ""
-          )}
-          onClick={(): void => {
-            this.props.toggleBag();
-          }}
+          className={cs(styles.bagBackdrop, show ? styles.active : "")}
+          // onClick={(): void => {
+
+          // }}
         ></div>
         <div
           className={cs(
             styles.bag,
-            { [styles.active]: this.props.active },
-            { [styles.smoothOut]: !this.props.active }
+            { [styles.active]: show },
+            { [styles.smoothOut]: !show }
           )}
         >
           <div
@@ -382,7 +143,7 @@ class CushionBag extends React.Component<Props, State> {
             <div className={styles.heading}>PURCHASE INSERT</div>
             <div
               className={globalStyles.pointer}
-              onClick={this.props.toggleBag}
+              // onClick={() => {}}
             >
               <i
                 className={cs(
@@ -393,7 +154,7 @@ class CushionBag extends React.Component<Props, State> {
               ></i>
             </div>
           </div>
-          <div className={styles.bagContents}>{this.getItems()}</div>
+          <div className={styles.bagContents}>{this.getsection()}</div>
           {this.getFooter()}
         </div>
       </div>
