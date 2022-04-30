cb4e59f0b5d27b6d6affbb940881909f2da99b87
Darpan Rawat <darpanrawat@goodearth.in>
2022-04-30 10:19:52 +0530

    gbp currency correct

--- src/constants/currency.ts
+++ src/constants/currency.ts
@@ -3,7 +3,7 @@ import { Currency } from "typings/currency";
 export const currencyCodes: { [key in Currency]: any } = {
   INR: [8377],
   USD: [36],
-  GBP: [65, 69, 68],
+  GBP: [163],
   AED: [65, 69, 68],
   SGD: [83, 36]
 };
--- src/typings/currency.ts
+++ src/typings/currency.ts
@@ -3,7 +3,7 @@ export type Currency = "USD" | "INR" | "GBP" | "AED" | "SGD";
 export const currencyCode: { [x in Currency]: any } = {
   INR: [8377],
   USD: [36],
-  GBP: [65, 69, 68],
+  GBP: [163],
   AED: [65, 69, 68],
   SGD: [83, 36]
 };
