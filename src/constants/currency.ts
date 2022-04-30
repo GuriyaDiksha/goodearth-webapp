486fd0ec91ad0b7bcb484c8e81322141a5a73b5a
Darpan Rawat <darpanrawat@goodearth.in>
2022-04-30 09:17:56 +0530

    code for currency fix in giftcard

--- src/constants/currency.ts
+++ src/constants/currency.ts
@@ -3,7 +3,7 @@ import { Currency } from "typings/currency";
 export const currencyCodes: { [key in Currency]: any } = {
   INR: [8377],
   USD: [36],
-  GBP: [163],
+  GBP: [65, 69, 68],
   AED: [65, 69, 68],
   SGD: [83, 36]
 };
