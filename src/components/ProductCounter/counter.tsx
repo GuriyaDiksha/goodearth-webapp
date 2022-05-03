da23170bcc5fb95afa8ddec8139e08bed98f3f5e
Darpan Rawat <darpanrawat@goodearth.in>
2022-05-03 12:24:54 +0530

    code for bug resolve cusion filler

--- src/components/ProductCounter/counter.tsx
+++ src/components/ProductCounter/counter.tsx
@@ -3,9 +3,14 @@ import { ProductCounterProps } from "./typings";
 import cs from "classnames";
 import styles from "./styles.scss";
 
-const Counter: React.FC<ProductCounterProps> = ({ current, total }) => {
+const Counter: React.FC<ProductCounterProps> = ({
+  current,
+  total,
+  isclass
+}) => {
+  const newclass = isclass ? isclass : styles.counterstyle;
   return (
-    <div className={cs(styles.counterstyle)}>
+    <div className={cs(newclass)}>
       <div className={cs(styles.counter)}>
         <div className={cs(styles.counterNumber)}>
           <div>{current}</div>/<div>{total}</div>
