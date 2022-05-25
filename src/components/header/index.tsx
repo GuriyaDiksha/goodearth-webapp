21ed2a617fc2cde48d536c44f2ea20a6af5e57a0
Saumya <saumyawardhan@goodearth.in>
2022-05-25 15:29:49 +0530

    In shop availability fix

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -190,6 +190,7 @@ class Header extends React.Component<Props, State> {
     const that = this;
     let previousUrl = "";
     const observer = new MutationObserver(function(mutations) {
+      console.log(that.props.showStock);
       if (location.href !== previousUrl) {
         previousUrl = location.href;
         that.setState({ showBag: false });
@@ -198,6 +199,9 @@ class Header extends React.Component<Props, State> {
         }
         if (that.props.openModal) {
           that.props.closeModal();
+        }
+        if (that.props.showStock) {
+          that.props.closeInShopAvailability();
         }
       }
     });
