569d266fffc7c4e5689635834716c1da41dc6a5b
Darpan Rawat <darpanrawat@goodearth.in>
2022-06-21 18:02:12 +0530

    code added for validate

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -945,17 +945,30 @@ export const checkoutGTM = (
   paymentMethod?: string
 ) => {
   const productList = productForBasketGa(basket, currency);
-  dataLayer.push({
-    event: "checkout",
-    ecommerce: {
-      currencyCode: currency,
-      paymentMethod,
-      checkout: {
-        actionField: { step, option: checkoutSteps[step - 1] },
-        products: productList
+  if (paymentMethod) {
+    dataLayer.push({
+      event: "checkout",
+      ecommerce: {
+        currencyCode: currency,
+        paymentMethod,
+        checkout: {
+          actionField: { step, option: checkoutSteps[step - 1] },
+          products: productList
+        }
       }
-    }
-  });
+    });
+  } else {
+    dataLayer.push({
+      event: "checkout",
+      ecommerce: {
+        currencyCode: currency,
+        checkout: {
+          actionField: { step, option: checkoutSteps[step - 1] },
+          products: productList
+        }
+      }
+    });
+  }
 };
 
 export const headerClickGTM = (
