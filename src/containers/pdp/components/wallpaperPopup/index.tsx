b824d0813bca5958c31c811ef08f73655b8f01cb
Saumya <saumyawardhan@goodearth.in>
2022-07-05 07:18:36 +0530

    Wallpaper popup styling

--- src/containers/pdp/components/wallpaperPopup/index.tsx
+++ src/containers/pdp/components/wallpaperPopup/index.tsx
@@ -1,12 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import cs from "classnames";
 // components
-import Button from "components/Button";
+import { Context } from "components/Modal/context";
 import CloseButton from "components/Modal/components/CloseButton";
 import InputField from "components/InputField";
 // styles
 import bootstrap from "styles/bootstrap/bootstrap-grid.scss";
 import styles from "./styles.scss";
+import fontStyles from "styles/iconFonts.scss";
 
 type Props = {
   price: number;
@@ -14,6 +15,8 @@ type Props = {
 };
 
 const WallpaperPopup: React.FC<Props> = ({ currency, price }) => {
+  const { closeModal } = useContext(Context);
+
   const [numOfRolls, setNumOfRolls] = useState(0);
 
   const [width, setWidth] = useState("");
@@ -80,50 +83,65 @@ const WallpaperPopup: React.FC<Props> = ({ currency, price }) => {
 
   return (
     <div className={cs(styles.container)}>
-      <div className={styles.title}>WALLCOVERING CALCULATOR</div>
-      <CloseButton className={styles.closeButton} />
-
-      <div className={styles.text}>
-        In order to help you with your wallcovering calculations simply measure
-        your walls and enter your measurements below to estimate how many
-        standard rolls of wallcovering you will need.
+      <div className={styles.closeBtnContainer}>
+        <span
+          className={cs(
+            styles.closeButton,
+            fontStyles.icon,
+            fontStyles.iconCross
+          )}
+          onClick={closeModal}
+        ></span>
       </div>
 
-      <InputField
-        id="width"
-        value={width}
-        onChange={onWidthChange}
-        validator={validator}
-        className={styles.field}
-        label="WIDTH"
-        placeholder="WIDTH (in ft)"
-        errorMsg={widthError}
-      />
+      <div className={styles.contentContainer}>
+        <div className={styles.title}>WALLCOVERING CALCULATOR</div>
 
-      <InputField
-        id="height"
-        value={height}
-        onChange={onHeightChange}
-        validator={validator}
-        className={styles.field}
-        label="HEIGHT"
-        placeholder="HEIGHT (in ft)"
-        errorMsg={heightError}
-      />
-
-      <Button label="Calculate" onClick={calculateQuantity} />
-      {numOfRolls ? (
-        <div className={cs(bootstrap.row, styles.result)}>
-          <div className={cs(bootstrap.col12, bootstrap.colMd6)}>
-            {numOfRolls} roll(s) needed
-          </div>
-          <div className={cs(bootstrap.col12, bootstrap.colMd6)}>
-            {currency} {price * numOfRolls}
-          </div>
+        <div className={styles.text}>
+          In order to help you with your wallcovering calculations simply
+          measure your walls and enter your measurements below to estimate how
+          many standard rolls of wallcovering you will need.
         </div>
-      ) : (
-        ""
-      )}
+
+        <InputField
+          id="width"
+          value={width}
+          onChange={onWidthChange}
+          validator={validator}
+          className={styles.field}
+          label="WIDTH"
+          placeholder="WIDTH (in ft)"
+          errorMsg={widthError}
+        />
+
+        <InputField
+          id="height"
+          value={height}
+          onChange={onHeightChange}
+          validator={validator}
+          className={styles.field}
+          label="HEIGHT"
+          placeholder="HEIGHT (in ft)"
+          errorMsg={heightError}
+        />
+        <div className={cs(styles.btnContainer)}>
+          <button className={styles.calculateBtn} onClick={calculateQuantity}>
+            Calculate
+          </button>
+        </div>
+        {numOfRolls ? (
+          <div className={cs(bootstrap.row, styles.result)}>
+            <div className={cs(bootstrap.col12, bootstrap.colMd6)}>
+              {numOfRolls} roll(s) needed
+            </div>
+            <div className={cs(bootstrap.col12, bootstrap.colMd6)}>
+              {currency} {price * numOfRolls}
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
     </div>
   );
 };
