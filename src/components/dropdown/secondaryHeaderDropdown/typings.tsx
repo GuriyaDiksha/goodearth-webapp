cee1d157141d4820d558e6a6450f3b2f1aa5cd66
Saumya <saumyawardhan@goodearth.in>
2022-07-14 17:52:30 +0530

    Secodnary Header Dropdown

---
+++ src/components/dropdown/secondaryHeaderDropdown/typings.tsx
@@ -0,0 +1,8 @@
+import { CategoryMenu } from "containers/collectionLanding/typings";
+
+export type SecondaryHeaderDropdownMenuProps = {
+  id: string;
+  onChange: (value?: string, label?: string) => void;
+  value: string;
+  items: CategoryMenu[];
+};
