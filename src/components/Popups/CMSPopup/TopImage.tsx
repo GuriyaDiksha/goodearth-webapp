78da8fbbd109ae6bb2e6def502e5b61ce2862024
shraddha <p-shraddhamodi@goodearth.in>
2022-08-17 11:13:28 +0530

    fix disclaimer issue

--- src/components/Popups/CMSPopup/TopImage.tsx
+++ src/components/Popups/CMSPopup/TopImage.tsx
@@ -65,7 +65,7 @@ const TopImage: React.FC<Props> = ({
               src={mobile || tablet ? imageMobile || image : image}
             />
           </div>
-          <div className={cs(styles.gcTnc)}>
+          <div className={cs(styles.gcTnc, styles.topImage)}>
             {/* {icon && <img src={icon} className={styles.icon} />} */}
             <div className={cs(globalStyles.popupHeading, styles.heading)}>
               {ReactHtmlParser(heading)}
