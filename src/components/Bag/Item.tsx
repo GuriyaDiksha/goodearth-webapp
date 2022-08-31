2f0d33adc0033b5f3333be99e2be040ecc4d1a1b
Darpan Rawat <darpanrawat@goodearth.in>
2022-08-31 16:38:56 +0530

    code added

--- src/components/Bag/Item.tsx
+++ src/components/Bag/Item.tsx
@@ -117,6 +117,34 @@ const LineItems: React.FC<BasketItem> = memo(
           }
         }
       });
+      dataLayer.push({ ecommerce: null }); // Clear the previous ecommerce object.
+      dataLayer.push({
+        event: "remove_from_cart",
+        ecommerce: {
+          items: [
+            {
+              item_id: product.sku || product.childAttributes[0].sku,
+              item_name: product.title,
+              affiliation: product.title,
+              coupon: "", // Pass the coupon if available
+              currency: currency, // Pass the currency code
+              discount: childAttributes[0]?.discountedPriceRecords[currency], // Pass the discount amount
+              index: "",
+              item_brand: "goodearth",
+              item_category: categoryname,
+              item_category2: size,
+              item_category3: "",
+              item_list_id: "",
+              item_list_name: "",
+              item_variant: "",
+              item_category4: product.categories[0],
+              item_category5: product.collection,
+              price: price,
+              quantity: quantity
+            }
+          ]
+        }
+      });
       const categoryList = product.categories
         ? product.categories.length > 0
           ? product.categories[product.categories.length - 1].replace(/>/g, "-")
