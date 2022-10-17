60b3a924d588c0bdbe368781b685c9be34d9e2e4
Saumya <saumyawardhan@goodearth.in>
2022-10-17 10:18:21 +0530

    Basic Layout

--- src/containers/customer/components/Static/cust.tsx
+++ src/containers/customer/components/Static/cust.tsx
@@ -1,14 +1,30 @@
 import React from "react";
+import Accordion from "components/Accordion";
 import styles from "../styles.scss";
 import { Props } from "../../typings";
 import ReactHtmlParser from "react-html-parser";
+import cs from "classnames";
 import { scrollToId, removeFroala } from "utils/validate";
+import { AccordionData } from "../../typings";
+import { Section } from "components/Accordion/typings";
+import faqStyles from "containers/pdp/components/WallpaperFAQ/styles.scss";
 
-export default class Cust extends React.Component<Props, { content: string }> {
+export default class Cust extends React.Component<
+  Props,
+  {
+    link: string;
+    content: string;
+    pageTitle: string;
+    accordionData: AccordionData[];
+  }
+> {
   constructor(props: Props) {
     super(props);
     this.state = {
-      content: ""
+      content: "",
+      pageTitle: "",
+      accordionData: [],
+      link: ""
     };
     props.setCurrentSection();
   }
@@ -16,18 +32,48 @@ export default class Cust extends React.Component<Props, { content: string }> {
   componentDidMount() {
     this.props.fetchTerms().then(res => {
       this.setState({
-        content: res.content
+        content: res.content,
+        pageTitle: res.pageTitle,
+        link: res.link,
+        accordionData: res.accordionData
       });
-      // for handling scroll to particular element with id
       scrollToId();
       removeFroala();
     });
   }
   render() {
+    const { pageTitle, content, accordionData } = this.state;
+
     return (
       <div>
         <div className={styles.terms}>
-          {ReactHtmlParser(this.state.content)}
+          <div className={styles.pageTitle}>{pageTitle}</div>
+          <div className={styles.description}>{ReactHtmlParser(content)}</div>
+          {accordionData.map(({ content, heading, isAccordion }, index) => {
+            if (isAccordion) {
+              const section = ([
+                { header: heading, body: ReactHtmlParser(content), id: index }
+              ] as unknown) as Section[];
+              return (
+                <Accordion
+                  sections={section}
+                  className={styles.accordionClass}
+                  headerClassName={styles.accordionHeader}
+                  openIconClassName={cs(faqStyles.horizontal, faqStyles.open)}
+                  closedIconClassName={cs(faqStyles.horizontal)}
+                />
+              );
+            } else {
+              return (
+                <div className={styles.noAccordion}>
+                  <div className={styles.contentHeading}>{heading}</div>
+                  <div className={styles.content}>
+                    {ReactHtmlParser(content)}
+                  </div>
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     );
