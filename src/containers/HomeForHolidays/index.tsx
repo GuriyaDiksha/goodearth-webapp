4c23b188d470379a6b60e93b4683b0f4d44d4044
Darpan Rawat <darpanrawat@goodearth.in>
2022-11-14 17:47:49 +0530

    code for sale for

--- src/containers/HomeForHolidays/index.tsx
+++ src/containers/HomeForHolidays/index.tsx
@@ -186,7 +186,7 @@ const NotificationForm: React.FC = () => {
     >
       <h4>
         Indulge in a season of merriment with your Good Earth favourites at upto
-        50% OFF. Sign up to get a reminder for our Home For the Holidays Sale.
+        50% OFF. Sign up to get a reminder for our Home for the Holidays Sale.
       </h4>
       <Formsy
         onValidSubmit={handleSubmit}
