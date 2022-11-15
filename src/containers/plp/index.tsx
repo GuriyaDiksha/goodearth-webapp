e14bd8442db7943f3d9934ecea33289f1d30e44d
Saumya Wardhan <saumyawardhan@goodearth.in>
2022-11-15 17:28:55 +0530

    Reset Filters Tile

--- src/containers/plp/index.tsx
+++ src/containers/plp/index.tsx
@@ -13,6 +13,7 @@ import CorporateFilter from "./corporateList";
 import PlpDropdownMenu from "components/PlpDropDown";
 import PlpResultItem from "components/plpResultItem";
 import GiftcardItem from "components/plpResultItem/giftCard";
+import ResetFiltersTile from "components/plpResultItem/resetFiltersTile";
 import PlpBreadcrumbs from "components/PlpBreadcrumbs";
 import mapDispatchToProps from "../../components/Modal/mapper/actions";
 import MakerEnhance from "maker-enhance";
@@ -791,11 +792,11 @@ class PLP extends React.Component<
                 })}
               >
                 <span>
-                  {count > 1
-                    ? (!this.state.corporoateGifting ? count + 1 : count) +
-                      " products found"
-                    : (!this.state.corporoateGifting ? count + 1 : count) +
-                      " product found"}{" "}
+                  {count > 0
+                    ? count > 1
+                      ? count + " products found"
+                      : count + " product found"
+                    : "No products found"}{" "}
                 </span>
               </div>
             ) : (
@@ -803,11 +804,11 @@ class PLP extends React.Component<
                 className={cs(styles.productNumber, styles.imageContainer, {})}
               >
                 <span>
-                  {count > 1
-                    ? (!this.state.corporoateGifting ? count + 1 : count) +
-                      " products found"
-                    : (!this.state.corporoateGifting ? count + 1 : count) +
-                      " product found"}{" "}
+                  {count > 0
+                    ? count > 1
+                      ? count + " products found"
+                      : count + " product found"
+                    : "No products found"}{" "}
                 </span>
               </div>
             )}
@@ -987,6 +988,35 @@ class PLP extends React.Component<
                   />
                 )}
               </div>
+
+              {count < 1 && this.state.filterCount > 0 ? (
+                <div
+                  className={
+                    !mobile || this.props.plpMobileView == "grid"
+                      ? cs(bootstrap.colLg4, bootstrap.col6, styles.setWidth, {
+                          ["product-container"]: !this.state.corporoateGifting
+                        })
+                      : cs(
+                          bootstrap.colLg4,
+                          bootstrap.col12,
+                          styles.setWidth,
+                          styles.listViewContainer,
+                          {
+                            ["product-container"]: !this.state.corporoateGifting
+                          }
+                        )
+                  }
+                  key={2}
+                >
+                  {this.state.corporoateGifting ? (
+                    ""
+                  ) : (
+                    <ResetFiltersTile resetFilters={this.child.clearFilter} />
+                  )}
+                </div>
+              ) : (
+                ""
+              )}
             </div>
           </div>
           {mobile && !tablet && (
