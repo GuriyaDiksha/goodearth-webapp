bfe20476529ed4c0fb416f699ec93bb2aa2767c3
Saumya Wardhan <saumyawardhan@goodearth.in>
2022-12-09 18:27:56 +0530

    Height to H1>

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -592,7 +592,11 @@ class Header extends React.Component<Props, State> {
       : false;
     return (
       <div className="">
-        {meta.h1Tag && <h1>{meta.h1Tag}</h1>}
+        {meta.h1Tag && (
+          <h1 style={mobile ? { height: "45px", maxHeight: "45px" } : {}}>
+            {meta.h1Tag}
+          </h1>
+        )}
         <Helmet defer={false}>
           <title>
             {meta.titleTag
