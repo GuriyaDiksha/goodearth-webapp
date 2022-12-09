a7f1461fa6cd72571e75dcff55029a689bce3952
Darpan Rawat <darpanrawat@goodearth.in>
2022-12-09 15:52:35 +0530

    error msg resolve

--- src/containers/myAccount/components/MyProfile/index.tsx
+++ src/containers/myAccount/components/MyProfile/index.tsx
@@ -172,7 +172,8 @@ class MyProfile extends React.Component<Props, State> {
           updateProfile: false
         });
       })
-      .catch(err => {
+      .catch(error => {
+        const errdata = valid.decriptdata(error.response?.data);
         this.setState(
           {
             // disableButton: false
@@ -181,7 +182,7 @@ class MyProfile extends React.Component<Props, State> {
             this.handleInvalidSubmit();
           }
         );
-        Object.keys(err.response.data).map(data => {
+        Object.keys(errdata).map(data => {
           switch (data) {
             case "firstName":
             case "lastName":
@@ -192,13 +193,13 @@ class MyProfile extends React.Component<Props, State> {
             case "panPassportNumber":
               updateInputsWithError(
                 {
-                  [data]: err.response.data[data][0]
+                  [data]: data[data][0]
                 },
                 true
               );
               break;
             case "error_message": {
-              let errorMsg = err.response.data[data][0];
+              let errorMsg = data[data][0];
               if (errorMsg == "MaxRetries") {
                 errorMsg =
                   "You have exceeded max attempts, please try after some time.";
