6cd8eca333b601773ec5a65815beda175d67b62f
Saumya Wardhan <saumyawardhan@goodearth.in>
2023-03-15 10:39:41 +0530

    Formsy Used

--- src/containers/myAccount/components/myPreferences/index.tsx
+++ src/containers/myAccount/components/myPreferences/index.tsx
@@ -1,13 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import AccountService from "services/account";
+import FormCheckbox from "components/Formsy/FormCheckbox";
 import styles from "../styles.scss";
 import globalStyles from "styles/global.scss";
 import cs from "classnames";
+import Formsy from "formsy-react";
 
 const MyPreferences = () => {
-  const [preferencesData, setPreferencesData] = useState({});
+  const [preferencesData, setPreferencesData] = useState<any>({});
+  const whatsappCheckRef = useRef<HTMLInputElement>(null);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -16,50 +19,83 @@ const MyPreferences = () => {
     });
   }, []);
 
+  const onSubmit = (model: any) => {
+    console.log(model);
+    console.log(whatsappCheckRef.current?.checked);
+    // const {subscribe}
+    // AccountService.updateAccountPreferences({
+    //   dispatch,
+    //   {
+    //     subscribe: subscribe,
+    //     whatsappNo: whatsappNo,
+    //     whatsappNoCountryCode: whatsappNoCountryCode,
+    //     whatsappSubscribe: whatsappSubscribe
+    //   }
+    // })
+  };
+
   return (
     <div className={styles.myPrefContainer}>
       <div className={styles.formHeading}>My Preferences</div>
       <div className={styles.formSubheading}>
         Manage your communication preferences.
       </div>
-      <div className={styles.content}>
-        <div></div>
-        <div className={styles.whatsapp}>
-          By checking this, you agree to receiving Whatsapp messages for order &
-          profile related information. To know more how we keep your data safe,
-          refer to our &nbsp;
-          <Link
-            key="privacy"
-            to="/customer-assistance/privacy-policy"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Privacy Policy
-          </Link>
-          .
-        </div>
-        <div className={styles.subscribe}>
-          By checking this, you agree to receiving Whatsapp messages for order &
-          profile related information. To know more how we keep your data safe,
-          refer to our &nbsp;
-          <Link
-            key="privacy"
-            to="/customer-assistance/privacy-policy"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Privacy Policy
-          </Link>
-          .
-        </div>
-        <div className={styles.savePrefBtn}>
-          <input
-            type="submit"
-            value="Save Preferences"
-            className={cs(globalStyles.charcoalBtn)}
+      <Formsy onSubmit={onSubmit}>
+        <div className={cs(styles.content, styles.categorylabel)}>
+          <FormCheckbox
+            id="whatsappSubscribe"
+            name="whatsappSubscribe"
+            disable={false}
+            label={["Subscribe me for Whatsapp updates"]}
+            value={preferencesData.whatsappSubscribe}
+            labelClassName={styles.checkboxLabel}
+            inputRef={whatsappCheckRef}
           />
+          <div className={styles.whatsapp}>
+            By checking this, you agree to receiving Whatsapp messages for order
+            & profile related information. To know more how we keep your data
+            safe, refer to our &nbsp;
+            <Link
+              key="privacy"
+              to="/customer-assistance/privacy-policy"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Privacy Policy
+            </Link>
+            .
+          </div>
+          <FormCheckbox
+            id="subscribe"
+            name="subscribe"
+            disable={false}
+            label={["Subscribe me to Emailers & Newsletters"]}
+            value={preferencesData.subscribe}
+            labelClassName={styles.checkboxLabel}
+          />
+          <div className={styles.subscribe}>
+            By checking this, you agree to receiving Whatsapp messages for order
+            & profile related information. To know more how we keep your data
+            safe, refer to our &nbsp;
+            <Link
+              key="privacy"
+              to="/customer-assistance/privacy-policy"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Privacy Policy
+            </Link>
+            .
+          </div>
+          <div className={styles.savePrefBtn}>
+            <input
+              type="submit"
+              value="Save Preferences"
+              className={cs(globalStyles.charcoalBtn)}
+            />
+          </div>
         </div>
-      </div>
+      </Formsy>
     </div>
   );
 };
