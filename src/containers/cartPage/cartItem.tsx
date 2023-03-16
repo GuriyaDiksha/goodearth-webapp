cef88ea81dc1bd0d3ddc199453fe6a6a7f046198
Shradha Modi <p-shraddhamodi@goodearth.in>
2023-03-16 11:46:58 +0530

    Bug-wishlist Solved

--- src/containers/cartPage/cartItem.tsx
+++ src/containers/cartPage/cartItem.tsx
@@ -218,7 +218,7 @@ const CartItems: React.FC<BasketItem> = memo(
           </div>
           {(mobile || tablet) && " "}
           <div
-            className={cs(styles.productSize, {
+            className={cs(styles.productSize, styles.productSizeValue, {
               [styles.inline]: mobile || tablet
             })}
           >
