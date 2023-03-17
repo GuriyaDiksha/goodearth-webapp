5fd7f75341e32ca9cd0d79aa4e679ef20c6ffa69
Saumya Wardhan <saumyawardhan@goodearth.in>
2023-03-17 14:10:30 +0530

    Temp

--- src/containers/myAccount/components/myPreferences/index.tsx
+++ src/containers/myAccount/components/myPreferences/index.tsx
@@ -1,22 +1,28 @@
 import React, { useEffect, useRef, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import AccountService from "services/account";
+import LoginService from "services/login";
 import FormCheckbox from "components/Formsy/FormCheckbox";
 import styles from "../styles.scss";
 import globalStyles from "styles/global.scss";
 import cs from "classnames";
 import Formsy from "formsy-react";
+import CountryCode from "components/Formsy/CountryCode";
+import FormInput from "components/Formsy/FormInput";
+import { updateCountryData } from "actions/address";
+import { AppState } from "reducers/typings";
 
-const WhatsappSubscribe = (data: any, ref: any) => {
-  const [showPhone, setShowPhone] = useState(false);
+const WhatsappSubscribe = (data: any, ref: any, isdList: any) => {
+  const [checked, setChecked] = useState(false);
 
   useEffect(() => {
-    if (ref.current?.checked) {
-      setShowPhone(true);
-    }
-    console.log(ref.current?.checked);
-  }, [ref.current?.checked]);
+    setChecked(data.whatsappSubscribe);
+  }, [data]);
+
+  const onChange = (e: any) => {
+    setChecked(e.target.checked);
+  };
 
   return (
     <div className={styles.whatsapp}>
@@ -28,8 +34,60 @@ const WhatsappSubscribe = (data: any, ref: any) => {
         value={data.whatsappSubscribe}
         labelClassName={styles.checkboxLabel}
         inputRef={ref}
+        handleChange={onChange}
       />
-      {showPhone && <div>Okay!!</div>}
+      {checked && (
+        <div className={styles.countryCode}>
+          <CountryCode
+            name="code"
+            placeholder="Code"
+            label="Country Code"
+            value=""
+            id="isdcode"
+            validations={{
+              isCodeValid: (values, value) => {
+                return !(values.phone && value == "");
+              },
+              isValidCode: (values, value) => {
+                if (value && isdList.length > 0) {
+                  return isdList.indexOf(value ? value : "") > -1;
+                } else {
+                  return true;
+                }
+              }
+            }}
+            validationErrors={{
+              isCodeValid: "Required",
+              isValidCode: "Enter valid code"
+            }}
+            showLabel={true}
+          />
+        </div>
+      )}
+      {checked && (
+        <FormInput
+          // required
+          name="phone"
+          value=""
+          placeholder={"Contact Number"}
+          type="number"
+          label={"Contact Number"}
+          validations={{
+            isExisty: true
+          }}
+          validationErrors={{
+            isExisty: "Please enter your Contact Number"
+          }}
+          keyPress={e => (e.key == "Enter" ? e.preventDefault() : "")}
+          keyDown={e => (e.which === 69 ? e.preventDefault() : null)}
+          onPaste={e =>
+            e?.clipboardData.getData("Text").match(/([e|E])/)
+              ? e.preventDefault()
+              : null
+          }
+          showLabel={true}
+        />
+      )}
       By checking this, you agree to receiving Whatsapp messages for order &
       profile related information. To know more how we keep your data safe,
       refer to our &nbsp;
@@ -47,15 +105,34 @@ const WhatsappSubscribe = (data: any, ref: any) => {
 };
 
 const MyPreferences = () => {
+  const { countryData } = useSelector((state: AppState) => state.address);
+
   const [preferencesData, setPreferencesData] = useState<any>({});
+  const [isdList, setIsdList] = useState<any>([]);
   const whatsappCheckRef = useRef<HTMLInputElement>(null);
   const dispatch = useDispatch();
 
+  const fetchCountryData = async () => {
+    const data = await LoginService.fetchCountryData(dispatch);
+    dispatch(updateCountryData(data));
+  };
+
   useEffect(() => {
+    if (countryData.length == 0) {
+      fetchCountryData();
+    }
+
     AccountService.fetchAccountPreferences(dispatch).then((data: any) => {
       setPreferencesData(data);
     });
   }, []);
+
+  useEffect(() => {
+    const isdList = countryData.map(list => {
+      return list.isdCode;
+    });
+    setIsdList(isdList);
+  }, [countryData]);
 
   const onSubmit = (model: any) => {
     console.log(model);
@@ -80,7 +157,7 @@ const MyPreferences = () => {
       </div>
       <Formsy onSubmit={onSubmit}>
         <div className={cs(styles.content, styles.categorylabel)}>
-          {WhatsappSubscribe(preferencesData, whatsappCheckRef)}
+          {WhatsappSubscribe(preferencesData, whatsappCheckRef, isdList)}
           <div className={styles.subscribe}>
             <FormCheckbox
               id="subscribe"
