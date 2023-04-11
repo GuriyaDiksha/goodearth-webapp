bb3373240d39f3d7ec20f2cb2bb584ea790f133e
shraddha modi <p-shraddhamodi@goodearth.in>
2023-04-11 11:25:10 +0530

    tulsi address fix for shipping

--- src/components/Address/AddressList/index.tsx
+++ src/components/Address/AddressList/index.tsx
@@ -64,10 +64,23 @@ const AddressList: React.FC<Props> = props => {
   }, [addressDataList]);
 
   useEffect(() => {
-    const stateIdList = addressData.map(e => e.id);
-    const storeIdList = addressDataList.map(e => e.id);
-    const diff = stateIdList.filter(e => !storeIdList.includes(e));
-    const newStateList = addressData.filter(e => e.id != diff[0]);
+    let newStateList;
+    if (
+      activeStep == "SHIPPING" &&
+      props.currentCallBackComponent == "checkout-shipping"
+    ) {
+      const stateIdList = addressData.map(e => e.id);
+      const storeIdList = addressDataList.map(e => e.id);
+      const diff = stateIdList.filter(e => !storeIdList.includes(e));
+      newStateList = addressData.filter(e => e.id != diff[0]);
+    } else {
+      const stateIdList = addressData.filter(e => !e.isTulsi).map(e => e.id);
+      const storeIdList = addressDataList
+        .filter(e => !e.isTulsi)
+        .map(e => e.id);
+      const diff = stateIdList.filter(e => !storeIdList.includes(e));
+      newStateList = addressData.filter(e => e.id != diff[0] && !e.isTulsi);
+    }
     setAddressData(newStateList);
   }, [addressDataList]);
 
