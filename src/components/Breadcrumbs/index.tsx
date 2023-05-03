669269055e781019b252db27a11a69d4731535f4
Saumya Wardhan <saumyawardhan@goodearth.in>
2023-05-03 12:45:37 +0530

    Comment Debugger

--- src/components/Breadcrumbs/index.tsx
+++ src/components/Breadcrumbs/index.tsx
@@ -6,7 +6,7 @@ import cs from "classnames";
 
 const PlpBreadcrumbs: React.FC<Props> = memo(
   ({ levels = [], separator = " > ", className }) => {
-    debugger;
+    // debugger;
     const breadcrumbs: ReactNode[] = [];
 
     levels.map(({ name, url }, index) => {
