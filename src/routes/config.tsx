6708da0158d4ce9948590cb9c3e9d3546938d39e
Darpan Rawat <darpanrawat@goodearth.in>
2023-05-25 13:50:56 +0530

    added page for smart nav inline

--- src/routes/config.tsx
+++ src/routes/config.tsx
@@ -271,14 +271,24 @@ const makerRoutes = [
   ROUTES.MIRAGESHOP,
   ROUTES.MAGHREB,
   ROUTES.TANGIER,
-  ROUTES.MAKERTEST,
   ROUTES.ANINDIANSUMMER
 ];
+
+const makerSmartnav = [ROUTES.MAKERTEST];
 
 makerRoutes.forEach(route => {
   routes.push({
     path: route,
     component: loadable(() => import("containers/makerpage")),
+    action: async () => null,
+    exact: true
+  });
+});
+
+makerSmartnav.forEach(route => {
+  routes.push({
+    path: route,
+    component: loadable(() => import("containers/smartNav")),
     action: async () => null,
     exact: true
   });
