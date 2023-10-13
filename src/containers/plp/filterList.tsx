643dcbd701d53f46a8d615527cbcc53347242199
shraddha modi <p-shraddhamodi@goodearth.in>
2023-10-13 11:51:53 +0530

    added event for material filter and modify event of category

--- src/containers/plp/filterList.tsx
+++ src/containers/plp/filterList.tsx
@@ -1443,11 +1443,15 @@ class FilterList extends React.Component<Props, State> {
       oldSelectedCategory: event.target.value
     });
     const userConsent = CookieService.getCookie("consent").split(",");
+    const val =
+      event.target.id.split(">")?.[2] !== undefined
+        ? ` -${event.target.id.split(">")?.[2]}`
+        : "";
     if (userConsent.includes(GA_CALLS)) {
       dataLayer.push({
         event: "Filter used",
         "Filter type": "Category",
-        "Filter value": event.target.value
+        "Filter value": event.target.value + val
       });
     }
 
@@ -1560,6 +1564,15 @@ class FilterList extends React.Component<Props, State> {
     this.setState({
       filter: filter
     });
+
+    const userConsent = CookieService.getCookie("consent").split(",");
+    if (userConsent.includes(GA_CALLS)) {
+      dataLayer.push({
+        event: "Filter used",
+        "Filter type": "Material",
+        "Filter value": event.target.value
+      });
+    }
     this.createUrlfromFilter();
     event.stopPropagation();
   };
