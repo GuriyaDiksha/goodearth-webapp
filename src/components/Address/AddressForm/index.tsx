6ceb50ec7017fe39b0ab285b9e133a4d69bdf47b
shraddha modi <p-shraddhamodi@goodearth.in>
2023-11-29 10:48:19 +0530

    disabled state

--- src/components/Address/AddressForm/index.tsx
+++ src/components/Address/AddressForm/index.tsx
@@ -761,7 +761,7 @@ const AddressForm: React.FC<Props> = props => {
                   name="state"
                   label={"State*"}
                   placeholder={"Select State*"}
-                  disable={isIndia && mode == "edit"}
+                  disable={isIndia}
                   options={stateOptions}
                   value={
                     addressData && !isCountryChanged ? addressData.state : ""
