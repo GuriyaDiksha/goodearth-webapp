e707e68f6e7fc5ed4b6d1d8f95c2e0acf30f9c9f
shraddha modi <p-shraddhamodi@goodearth.in>
2024-01-22 13:53:35 +0530

    revert changes

--- src/containers/search/initAction.ts
+++ src/containers/search/initAction.ts
@@ -2,7 +2,10 @@ import { InitAction } from "typings/actions";
 import SearchService from "services/search";
 
 const initActionSearch: InitAction = async (store, params, { search }) => {
-  await SearchService.onLoadSearchPage(store.dispatch, search).catch(err => {
+  await SearchService.onLoadSearchPage(
+    store.dispatch,
+    search + `&page_size=${40}`
+  ).catch(err => {
     console.log("Search Error API error", err);
   });
 };
