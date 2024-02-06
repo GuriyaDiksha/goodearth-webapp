8b203583e54257372808ec451af5b22c25b59db6
guriya_diksha <guriyaofficial12@gmail.com>
2024-02-06 14:34:05 +0530

    removed console log statement

--- src/containers/cartPage/cartItem.tsx
+++ src/containers/cartPage/cartItem.tsx
@@ -516,12 +516,6 @@ const CartItems: React.FC<BasketItem> = memo(
                           )}
                         </div>
 
-                        {console.log(
-                          "check====" + saleStatus,
-                          childAttributes[0].showStockThreshold &&
-                            childAttributes[0].stock > 0,
-                          childAttributes[0].othersBasketCount > 0
-                        )}
                         {saleStatus &&
                           ((childAttributes[0].showStockThreshold &&
                             childAttributes[0].stock > 0 &&
