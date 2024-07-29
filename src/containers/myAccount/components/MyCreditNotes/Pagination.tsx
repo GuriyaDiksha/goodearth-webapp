08925993bad724dba584c9590cecc9be8ec4e154
Rushabh Dave <p-rushabhdave@goodearth.in>
2024-07-29 11:48:17 +0530

    moving back to page and not able to eaxpand

--- src/containers/myAccount/components/MyCreditNotes/Pagination.tsx
+++ src/containers/myAccount/components/MyCreditNotes/Pagination.tsx
@@ -7,6 +7,7 @@ type Props = {
   count: number;
   page: number;
   fetchPaginatedData: (z: number) => void;
+  collapseExpandItemOnPageChange: () => void;
 };
 
 const Pagination: React.FC<Props> = ({
@@ -14,6 +15,7 @@ const Pagination: React.FC<Props> = ({
   next,
   count,
   fetchPaginatedData,
+  collapseExpandItemOnPageChange,
   page
 }) => {
   return (
@@ -21,20 +23,33 @@ const Pagination: React.FC<Props> = ({
       <div className={styles.pagination}>
         <p
           className={previous ? "" : styles.inactive}
-          onClick={() => previous && fetchPaginatedData(page - 1)}
+          onClick={() => {
+            if (previous) {
+              fetchPaginatedData(page - 1);
+              collapseExpandItemOnPageChange();
+            }
+          }}
         ></p>
         {[...Array(Number(Math.ceil(count / 5))).keys()].map((ele, ind) => (
           <p
             key={ind}
             className={page === ele + 1 ? styles.active : ""}
-            onClick={() => fetchPaginatedData(ele + 1)}
+            onClick={() => {
+              fetchPaginatedData(ele + 1);
+              collapseExpandItemOnPageChange();
+            }}
           >
             {ele + 1}
           </p>
         ))}
         <p
           className={next ? "" : styles.inactive}
-          onClick={() => next && fetchPaginatedData(page + 1)}
+          onClick={() => {
+            if (next) {
+              fetchPaginatedData(page + 1);
+              collapseExpandItemOnPageChange();
+            }
+          }}
         ></p>
       </div>
     </div>
