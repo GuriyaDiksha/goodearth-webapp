ab5bdce07982f469b6a30d6621a9027cf5330fcc
Rushabh Dave <p-rushabhdave@goodearth.in>
2024-08-20 11:47:15 +0530

    changes done

--- src/containers/plp/components/PlpBubbles.tsx
+++ src/containers/plp/components/PlpBubbles.tsx
@@ -1,6 +1,6 @@
 import Slider from "react-slick";
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import styles from "../styles.scss";
 type Props = {
   facets: object;
   bubbleTitle: any;
@@ -8,59 +8,90 @@ type Props = {
 };
 type Item = [string, ...any[]];
 
-const PlpBubbles: React.FC<Props> = ({ bubbleTitle, facets, categoryShop }) => {
+const PlpBubbles: React.FC<Props> = ({ bubbleTitle, categoryShop }) => {
+  const [plpData, setData] = useState([]);
   const settings = {
     speed: 500,
-    slidesToShow: 5,
+    slidesToShow: 7,
     slidesToScroll: 1,
-    arrows: false,
-    centerMode: true, // enable center mode
-    centerPadding: "50px" // set center padding
+
+    infinite: false,
+    centerPadding: "50px", // set center padding
+    nextArrow: (
+      <div>
+        <div className="next-slick-arrow">&gt;</div>
+      </div>
+    ),
+    prevArrow: (
+      <div>
+        <div className="prev-slick-arrow"> &lt;</div>
+      </div>
+    ),
+    responsive: [
+      {
+        breakpoint: 1132,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 717,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 717,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
 
-  console.log(
-    "Bubble Title bbb ---> ",
-    bubbleTitle?.categoryObj?.[categoryShop.trim()]
-  );
-  const data: Item[] = bubbleTitle?.categoryObj?.[categoryShop.trim()] || [];
-  const elements = Array.isArray(data) ? data.map(item => item[0].trim()) : [];
-  console.log(elements);
+  useEffect(() => {
+    const data: Item[] = bubbleTitle?.categoryObj?.[categoryShop.trim()] || [];
+    const elements: any = Array.isArray(data)
+      ? data.map(item => item[0].trim())
+      : [];
+    setData(elements);
+  }, [bubbleTitle]);
 
   return (
-    <Slider {...settings}>
-      {elements.map((item, index) => (
-        <div
-          style={{
-            borderRadius: "50%",
-            maxHeight: "97px",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            color: "white",
-            backgroundColor: "pink",
-            width: "fit-content"
-          }}
-          key={index}
-        >
-          <img
-            src="https://static8.depositphotos.com/1020341/896/i/950/depositphotos_8969502-stock-photo-human-face-with-cracked-texture.jpg"
-            alt="img"
-            style={{
-              borderRadius: "50%",
-              width: "97px",
-              height: "97px",
-              objectFit: "cover"
-            }}
-          />
-          <span
-            style={{ color: "#000", fontSize: "12px", textAlign: "center" }}
-          >
-            {item}
-          </span>
-        </div>
-      ))}
-    </Slider>
+    <div className={styles.sliderContainer}>
+      <Slider {...settings}>
+        {plpData.map((item, index) => (
+          <div className={styles.bubbleContainer} key={index}>
+            <div>
+              <img
+                className={styles.bubbleImage}
+                src="https://static8.depositphotos.com/1020341/896/i/950/depositphotos_8969502-stock-photo-human-face-with-cracked-texture.jpg"
+                alt="img"
+              />
+            </div>
+
+            <span className={styles.bubbleText}>{item}</span>
+          </div>
+        ))}
+      </Slider>
+    </div>
   );
 };
 
