5b89a24e9169ae1ff19ce269b3a3355730944dfd
Ashhar Ali <ashharali@goodearth.in>
2024-10-10 19:05:53 +0530

    fix the handlepaste

--- src/components/signin/Login/mainLogin.tsx
+++ src/components/signin/Login/mainLogin.tsx
@@ -482,15 +482,6 @@ class MainLogin extends React.Component<Props, loginState> {
               showerror: ""
             });
           }
-        } else if (this.state.email?.trim() !== this.state.email) {
-          if (this.state.msg !== "Please enter a valid Email ID") {
-            this.setState({
-              msg: "Please enter a valid Email ID",
-              highlight: true,
-              showerror: "",
-              isLoginDisabled: true
-            });
-          }
         } else {
           if (this.state.msg !== "") {
             this.setState({
@@ -530,24 +521,23 @@ class MainLogin extends React.Component<Props, loginState> {
       case "email": {
         this.disablePassword();
         const pasteTxt = event.clipboardData.getData("text");
-        if (pasteTxt.length !== pasteTxt.trim().length) {
-          event.preventDefault();
-          return false;
-        }
         if (!checkMail(pasteTxt)) {
           if (this.state.msg !== "Please enter a valid Email ID") {
             this.setState({
               msg: "Please enter a valid Email ID",
               highlight: true,
-              showerror: ""
+              showerror: "",
+              email: pasteTxt.trim()
             });
           }
         } else {
           this.setState({
             showerror: "",
-            isLoginDisabled: false
+            isLoginDisabled: false,
+            email: pasteTxt.trim()
           });
         }
+        event.preventDefault();
         break;
       }
       case "password": {
