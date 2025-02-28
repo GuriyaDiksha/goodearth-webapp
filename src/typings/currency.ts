39fbddc265b78eb30b96e818c7a0e191ebdeffbb
guriya_diksha <guriyaofficial12@gmail.com>
2025-02-28 10:54:00 +0530

    AED closeer fixes

--- src/typings/currency.ts
+++ src/typings/currency.ts
@@ -1,10 +1,9 @@
-// export type Currency = "USD" | "INR" | "GBP" | "AED" | "SGD";
-export type Currency = "USD" | "INR" | "GBP" | "SGD";
+export type Currency = "USD" | "INR" | "GBP" | "AED" | "SGD";
 
 export const currencyCode: { [x in Currency]: any } = {
   INR: [8377],
   USD: [36],
   GBP: [163],
-  // AED: [65, 69, 68],
+  AED: [65, 69, 68],
   SGD: [83, 36]
 };
