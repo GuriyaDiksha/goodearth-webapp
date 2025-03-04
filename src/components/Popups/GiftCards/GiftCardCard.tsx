a733f6f65b42f3fad631cda9416833e21a5d5232
Your Name <your-email@example.com>
2025-03-04 16:45:59 +0530

    add scroll into view in error when we are aplying gc

--- src/components/Popups/GiftCards/GiftCardCard.tsx
+++ src/components/Popups/GiftCards/GiftCardCard.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, useEffect } from "react";
+import React, { forwardRef, useEffect, useRef } from "react";
 import cs from "classnames";
 import styles from "./index.scss";
 import { GiftCard } from "containers/myAccount/components/MyCreditNotes/typings";
@@ -40,7 +40,7 @@ const GiftCardCard = forwardRef<Props, any>(
     const mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
     const yyyy = today.getFullYear();
     const today_in_str = dd + "/" + mm + "/" + yyyy;
-
+    const errorRef = useRef<HTMLParagraphElement | null>(null);
     // useEffect(() => {
     //   // Scroll to the first element whose has darkBorder class
     //   const activeElement = document.getElementsByClassName(
@@ -51,6 +51,16 @@ const GiftCardCard = forwardRef<Props, any>(
     //     activeElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
     //   }
     // }, []);
+
+    useEffect(() => {
+      if (error?.[entry_code] && errorRef.current) {
+        errorRef.current.scrollIntoView({
+          behavior: "smooth",
+          block: "center"
+        });
+        errorRef.current.focus();
+      }
+    }, [error, entry_code]);
 
     return (
       <>
@@ -142,7 +152,9 @@ const GiftCardCard = forwardRef<Props, any>(
             </div>
           )}
         </div>
-        <p className={styles.errorMsg}>{error?.[entry_code]}</p>
+        <p ref={errorRef} tabIndex={-1} className={styles.errorMsg}>
+          {error?.[entry_code]}
+        </p>
       </>
     );
   }
